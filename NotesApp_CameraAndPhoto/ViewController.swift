2fb19c040fb5ff54c0c4e08d6ba7f5385aefffc1
Ella Madalinski <ella.madalinski24@d155.org>
2021-10-28 08:08:28 -0500

    finished 1/4 videos

--- NotesApp_CameraAndPhoto/ViewController.swift
+++ NotesApp_CameraAndPhoto/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageViewOutlet: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func photoAction(_ sender: UIButton) {
+    }
+    
+    @IBAction func cameraAction(_ sender: UIButton) {
+    }
+    
 }
 
